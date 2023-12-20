9a750c7b3a533223bc153b8e62cf55da98e7ede5
Hamda khalil <150124957+Hamdakhalil@users.noreply.github.com>
2023-12-20 19:10:13 +0500

    Update form.js

--- form.js
+++ form.js
@@ -1,55 +1,56 @@
+
 function validateForm() {
-  var NameInput = document.getElementById("Name");
-  var NameValue = NameInput.value.trim();
-  if (NameValue == "" || NameValue == null) {
-      alert("Please enter your name");
-      NameInput.focus();
-      return false;
-  }
+    var NameInput = document.getElementById("Name");
+    var NameValue = NameInput.value.trim();
+    if (NameValue == "" || NameValue == null) {
+        alert("Please enter your name");
+        NameInput.focus();
+        return false;
+    }
 
-  var emailInput = document.getElementById("email");
-  var emailValue = emailInput.value.trim();
-  var atPosition = emailValue.indexOf("@");
-  var dotPosition = emailValue.lastIndexOf(".");
+    var emailInput = document.getElementById("email");
+    var emailValue = emailInput.value.trim();
+    var atPosition = emailValue.indexOf("@");
+    var dotPosition = emailValue.lastIndexOf(".");
 
-  if (emailValue === "") {
-      alert("Please enter your email");
-      emailInput.focus();
-      return false;
-  }
+    if (emailValue === "") {
+        alert("Please enter your email");
+        emailInput.focus();
+        return false;
+    }
 
-  if (atPosition < 1 || dotPosition - atPosition < 2) {
-      alert("Please enter correct email ID");
-      emailInput.focus();
-      return false;
-  }
+    if (atPosition < 1 || dotPosition - atPosition < 2) {
+        alert("Please enter correct email ID");
+        emailInput.focus();
+        return false;
+    }
 
-  var passwordInput = document.getElementById("password");
-  var passwordValue = passwordInput.value.trim();
-  if (passwordValue == "" || passwordValue == null) {
-      alert("Please enter your password");
-      passwordInput.focus();
-      return false;
-  }
+    var passwordInput = document.getElementById("password");
+    var passwordValue = passwordInput.value.trim();
+    if (passwordValue == "" || passwordValue == null) {
+        alert("Please enter your password");
+        passwordInput.focus();
+        return false;
+    }
 
-  var male = document.getElementById("male");
-  var female = document.getElementById("female");
-  if (!male.checked || !female.checked) {
-      alert("Please select your gender");
-      return false;
-  }
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+    if (!male.checked || !female.checked) {
+        alert("Please select your gender");
+        return false;
+    }
 
-  var course = document.getElementById("course");
-  if (course.value == "") {
-      alert("Please select your course");
-      return false;
-  }
+    var course = document.getElementById("course");
+    if (course.value == "") {
+        alert("Please select your course");
+        return false;
+    }
 
-  var city = document.getElementById("city");
-  if (city.value == "") {
-      alert("Please select your city");
-      return false;
-  }
+    var city = document.getElementById("city");
+    if (city.value == "") {
+        alert("Please select your city");
+        return false;
+    }
 
-  return true;
+    return true;
 }
